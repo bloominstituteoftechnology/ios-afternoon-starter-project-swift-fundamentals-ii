252891ee01df3a11239af594b7a1318d13860b18
RoastBeefKazenakis <sabinowitz@gmail.com>
2019-09-05 17:22:08 -0400

    progress

--- AirportDepartures.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ AirportDepartures.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,47 +1,76 @@
 import UIKit
 
+enum FlightStatus: String {
+    case EnRoute = "En Route"
+    case Scheduled = "Scheduled"
+    case Canceled = "Canceled"
+    case Delayed = "Delayed"
+}
 
-//: ## 1. Create custom types to represent an Airport Departures display
-//: ![Airport Departures](matthew-smith-5934-unsplash.jpg)
-//: Look at data from [Departures at JFK Airport in NYC](https://www.airport-jfk.com/departures.php) for reference.
-//:
-//: a. Use an `enum` type for the FlightStatus (En Route, Scheduled, Canceled, Delayed, etc.)
-//:
-//: b. Use a struct to represent an `Airport` (Destination or Arrival)
-//:
-//: c. Use a struct to represent a `Flight`.
-//:
-//: d. Use a `Date?` for the departure time since it may be canceled.
-//:
-//: e. Use a `String?` for the Terminal, since it may not be set yet (i.e.: waiting to arrive on time)
-//:
-//: f. Use a class to represent a `DepartureBoard` with a list of departure flights, and the current airport
+let enRoute = FlightStatus.EnRoute
+let scheduled = FlightStatus.Scheduled
+let canceled = FlightStatus.Canceled
+let delayed = FlightStatus.Delayed
 
+enum AirportTerminal {
+    case Arrivals
+    case Departures
+}
+let arrivals = AirportTerminal.Arrivals
+let departures = AirportTerminal.Departures
 
+struct Airport {
+    let airportTerminal: AirportTerminal
+    
+}
 
-//: ## 2. Create 3 flights and add them to a departure board
-//: a. For the departure time, use `Date()` for the current time
-//:
-//: b. Use the Array `append()` method to add `Flight`'s
-//:
-//: c. Make one of the flights `.canceled` with a `nil` departure time
-//:
-//: d. Make one of the flights have a `nil` terminal because it has not been decided yet.
-//:
-//: e. Stretch: Look at the API for [`DateComponents`](https://developer.apple.com/documentation/foundation/datecomponents?language=objc) for creating a specific time
-
-
-
-//: ## 3. Create a free-standing function that can print the flight information from the `DepartureBoard`
-//: a. Use the function signature: `printDepartures(departureBoard:)`
-//:
-//: b. Use a `for in` loop to iterate over each departure
-//:
-//: c. Make your `FlightStatus` enum conform to `String` so you can print the `rawValue` String values from the `enum`. See the [enum documentation](https://docs.swift.org/swift-book/LanguageGuide/Enumerations.html).
-//:
-//: d. Print out the current DepartureBoard you created using the function
+struct Flights {
+    var FlightStatus: FlightStatus
+    var FlightTime: Date?
+    var Terminal: String?
+    
+    
+}
+let flight01 = Flights(FlightStatus: .Canceled, FlightTime: nil, Terminal: "a1")
+let flight02 = Flights(FlightStatus: .Delayed, FlightTime: Date(), Terminal: "b2")
+let flight03 = Flights(FlightStatus: .EnRoute, FlightTime: Date(), Terminal: "g1")
 
 
+class DepartureBoard {
+    var departureFlights: [Flights]
+    var currentAirport: String
+    
+    init (currentAirport: String = "JFK") {
+        self.currentAirport = currentAirport
+        departureFlights = []
+    }
+    func add(Flights: Flights) {
+        departureFlights.append(Flights)
+    }
+    
+    
+
+    
+    
+}
+func printDepartures(departureBoard:DepartureBoard) {
+    for departures in departureBoard.departureFlights {
+        var departureString: String = ""
+        if let departureTime = departures.FlightTime{
+            departureString = "\(departures.FlightTime)"
+        }
+        print(departures.FlightStatus)
+        
+    
+    }
+    
+}
+    let departure = DepartureBoard()
+print("destination: \(departure.currentAirport), airline: \(departure.)")
+
+printDepartures(departureBoard: departure)
+
+// printDepartures(departureBoard: departure.departureFlights)
 
 
 //: ## 4. Make a second function to print print an empty string if the `departureTime` is nil
@@ -96,6 +125,7 @@ import UIKit
 //: e. Make sure to cast the numbers to the appropriate types so you calculate the correct airfare
 //:
 //: f. Stretch: Use a [`NumberFormatter`](https://developer.apple.com/documentation/foundation/numberformatter) with the `currencyStyle` to format the amount in US dollars.
+
 
 
 

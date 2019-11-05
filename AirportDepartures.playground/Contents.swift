e35887cdbb38c0e61fd0b99f31455834efaa6f89
Jeremy Taylor <jtaylor@jctaylor.org>
2019-11-05 11:44:15 -0600

    Added FlightStatus Enum

--- AirportDepartures.playground/Contents.swift
+++ AirportDepartures.playground/Contents.swift
@@ -16,7 +16,13 @@ import UIKit
 //: e. Use a `String?` for the Terminal, since it may not be set yet (i.e.: waiting to arrive on time)
 //:
 //: f. Use a class to represent a `DepartureBoard` with a list of departure flights, and the current airport
-
+enum FlightStatus: String {
+    case scheduled = "Scheduled"
+    case delayed = "Delayed"
+    case cancelled = "Cancelled"
+    case onTime = "On Time"
+    case enRoute = "En Route"
+}
 
 
 //: ## 2. Create 3 flights and add them to a departure board

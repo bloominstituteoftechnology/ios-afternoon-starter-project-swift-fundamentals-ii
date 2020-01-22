bcf3b84f55f079cefa4c7d39536194c6c24243cd
ldmayfield <ldonnamayfield@gmail.com>
2020-01-21 22:27:26 -0700

    Exercises 1, 2, 3, 6 with stretch goals.

--- AirportDepartures.playground/Contents.swift
+++ AirportDepartures.playground/Contents.swift
@@ -16,7 +16,39 @@ import UIKit
 //: e. Use a `String?` for the Terminal, since it may not be set yet (i.e.: waiting to arrive on time)
 //:
 //: f. Use a class to represent a `DepartureBoard` with a list of departure flights, and the current airport
+enum FlightStatus: String {
+    case enRoute
+    case scheduled
+    case canceled
+    case delayed
+    case landed
+    case diverted
+    case boarding
+}
 
+struct Airport {
+    let city: String
+    let abbreviation: String
+}
+
+struct Flight {
+    var destination: String
+    var flightNo: String
+    var airline: String
+    var departTime: Date?
+    var terminal: String?
+    var flightStatus: FlightStatus
+}
+
+class DepartureBoard {
+    var flights: [Flight]
+    var airport: String
+    
+    init(Airport: String) {
+        self.airport = Airport
+        self.flights = []
+    }
+}
 
 
 //: ## 2. Create 3 flights and add them to a departure board
@@ -29,7 +61,37 @@ import UIKit
 //: d. Make one of the flights have a `nil` terminal because it has not been decided yet.
 //:
 //: e. Stretch: Look at the API for [`DateComponents`](https://developer.apple.com/documentation/foundation/datecomponents?language=objc) for creating a specific time
+let today = Date()
+let format = DateFormatter()
+format.dateStyle = .medium
+print(format.string(from: today))
 
+
+let flight1 = Flight(destination: "LAX",
+                     flightNo: "9K101",
+                     airline: "Cape Air",
+                     departTime: Date(),
+                     terminal: nil,
+                     flightStatus: .landed)
+
+let flight2 = Flight(destination: "ONT",
+                     flightNo: "B65921",
+                     airline: "JetBlue",
+                     departTime: Date(),
+                     terminal: "5",
+                     flightStatus: .enRoute)
+
+let flight3 = Flight(destination: "VIA",
+                     flightNo: "KE82",
+                     airline: "Korean Air",
+                     departTime: nil,
+                     terminal: nil,
+                     flightStatus: .canceled)
+
+let departureBoard = DepartureBoard(Airport: "LAX")
+departureBoard.flights.append(flight1)
+departureBoard.flights.append(flight2)
+departureBoard.flights.append(flight3)
 
 
 //: ## 3. Create a free-standing function that can print the flight information from the `DepartureBoard`
@@ -40,9 +102,14 @@ import UIKit
 //: c. Make your `FlightStatus` enum conform to `String` so you can print the `rawValue` String values from the `enum`. See the [enum documentation](https://docs.swift.org/swift-book/LanguageGuide/Enumerations.html).
 //:
 //: d. Print out the current DepartureBoard you created using the function
+func printDepartures(departureBoard: DepartureBoard) {
+    //for in loop to iterate over each departure
+    for flight in departureBoard.flights {
+        print("Destination: \(flight.destination), Airline: \(flight.airline), Departs: \(String(describing: flight.departTime)), FlightNo: \(flight.flightNo), FlightStatus: \(flight.flightStatus)")
+    }
+}
 
-
-
+printDepartures(departureBoard: departureBoard)
 
 //: ## 4. Make a second function to print print an empty string if the `departureTime` is nil
 //: a. Createa new `printDepartures2(departureBoard:)` or modify the previous function
@@ -96,6 +163,15 @@ import UIKit
 //: e. Make sure to cast the numbers to the appropriate types so you calculate the correct airfare
 //:
 //: f. Stretch: Use a [`NumberFormatter`](https://developer.apple.com/documentation/foundation/numberformatter) with the `currencyStyle` to format the amount in US dollars.
+var costPerBag = 25.00
+var costPerMile = 0.10
 
 
+func calcFare(checkedBags: Double, distance: Double, travelers: Int) -> Double {
+    let ticketCost = (checkedBags * costPerBag) + (distance * costPerMile)
+    let totalFare = ticketCost * Double(travelers)
+    return totalFare
+}
 
+print(calcFare(checkedBags: 10, distance: 1000, travelers: 5))
+print(calcFare(checkedBags: 4, distance: 3000, travelers: 2))

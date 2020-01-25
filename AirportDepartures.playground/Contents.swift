fda119acfb22cbd370c96f43eb8e6762eaea1020
Sam Gutierrez <Samguti02@gmail.com>
2020-01-25 15:18:39 -0700

    Finally got unstuck and made it halfway to step 3. Still working on my free standing function

--- AirportDepartures.playground/Contents.swift
+++ AirportDepartures.playground/Contents.swift
@@ -29,21 +29,24 @@ struct Airport {
 }
 
 struct Flight {
-    var flightName: String
-    var airline: String
-    var departureTime: Date?
-    var terminal: String?
+    let flightName: String
+    let airline: String
+    let departureTime: Date?
+    let terminal: String?
+    let flightStatus: flightStatus
+    let destination: Airport
 }
 
 class DepartureBoard {
-    var flight: [Flight]
-    var airport: String
+    var flights: [Flight]
+    var currentAirport: Airport
     
-    init(flight: [Flight], airport: String) {
-        self.flight = []
-        self.airport = airport
+    init(currentAirport: String) {
+        self.currentAirport = Airport(destination: "Georgetown")
+        self.flights = []
     }
 }
+
 //: ## 2. Create 3 flights and add them to a departure board
 //: a. For the departure time, use `Date()` for the current time
 //:
@@ -55,18 +58,33 @@ class DepartureBoard {
 //:
 //: e. Stretch: Look at the API for [`DateComponents`](https://developer.apple.com/documentation/foundation/datecomponents?language=objc) for creating a specific time
 
-//var flight1 = DepartureBoard.init(flight: [Flight(flightName: "AA2896", airline: "American Airlines", departureTime: Date(), terminal: nil)], airport: "JFK Airport")
-//var flight2 = DepartureBoard.init(flight: [Flight(flightName: "QF12", airline: "Qantas", departureTime: nil, terminal: nil)], airport: "JFK Airport")
-//var flight3 = DepartureBoard(flight: [Flight (flightName: "AY5747", airline: "Finnair", departureTime: Date(), terminal: "8")], airport: "JFK Airport")
 
-// There should be a way to add "JFK Airport" as an initializer. How would I do that? Should I change the Airport Struct?
+let flight1 = Flight(flightName: "AA2896",
+                     airline: "American Airlines",
+                     departureTime: Date(),
+                     terminal: nil,
+                     flightStatus: .EnRoute,
+                     destination: Airport(destination: "Georgetown"))
 
-var flight1 = Flight(flightName: "AA2896", airline: "American Airlines", departureTime: Date(), terminal: nil)
-var flight2 = Flight(flightName: "QF12", airline: "Qantas", departureTime: nil, terminal: nil)
-var flight3 = Flight(flightName: "AY5747", airline: "Finnair", departureTime: Date(), terminal: "8")
+let flight2 = Flight(flightName: "QF12",
+                     airline: "Qantas",
+                     departureTime: nil,
+                     terminal: nil,
+                     flightStatus: .Canceled,
+                     destination: Airport(destination: "Los Angeles"))
 
+let flight3 = Flight(flightName: "AY5747",
+                     airline: "Finnair",
+                     departureTime: Date(),
+                     terminal: "8",
+                     flightStatus: .EnRoute,
+                     destination: Airport(destination: "San Fransisco"))
 
+let departureBoard = DepartureBoard(currentAirport: "JFK Airport")
 
+departureBoard.flights.append(flight1)
+departureBoard.flights.append(flight2)
+departureBoard.flights.append(flight3)
 
 //: ## 3. Create a free-standing function that can print the flight information from the `DepartureBoard`
 //: a. Use the function signature: `printDepartures(departureBoard:)`
@@ -76,26 +94,15 @@ var flight3 = Flight(flightName: "AY5747", airline: "Finnair", departureTime: Da
 //: c. Make your `FlightStatus` enum conform to `String` so you can print the `rawValue` String values from the `enum`. See the [enum documentation](https://docs.swift.org/swift-book/LanguageGuide/Enumerations.html).
 //:
 //: d. Print out the current DepartureBoard you created using the function
+func printDepartures(departureBoard: DepartureBoard) {
+    for departure in departureBoard.flights {
+        print(departure)
+    }
+}
 
+printDepartures(departureBoard: departureBoard)
 
-
-
-//: ## 4. Make a second function to print print an empty string if the `departureTime` is nil
-//: a. Createa new `printDepartures2(departureBoard:)` or modify the previous function
-//:
-//: b. Use optional binding to unwrap any optional values, use string interpolation to turn a non-optional date into a String
-//:
-//: c. Call the new or udpated function. It should not print `Optional(2019-05-30 17:09:20 +0000)` for departureTime or for the Terminal.
-//:
-//: d. Stretch: Format the time string so it displays only the time using a [`DateFormatter`](https://developer.apple.com/documentation/foundation/dateformatter) look at the `dateStyle` (none), `timeStyle` (short) and the `string(from:)` method
-//:
-//: e. Your output should look like:
-//:
-//:     Destination: Los Angeles Airline: Delta Air Lines Flight: KL 6966 Departure Time:  Terminal: 4 Status: Canceled
-//:     Destination: Rochester Airline: Jet Blue Airways Flight: B6 586 Departure Time: 1:26 PM Terminal:  Status: Scheduled
-//:     Destination: Boston Airline: KLM Flight: KL 6966 Departure Time: 1:26 PM Terminal: 4 Status: Scheduled
-
-
+// Not done with question 3. Keep working on it!
 
 //: ## 5. Add an instance method to your `DepatureBoard` class (above) that can send an alert message to all passengers about their upcoming flight. Loop through the flights and use a `switch` on the flight status variable.
 //: a. If the flight is canceled print out: "We're sorry your flight to \(city) was canceled, here is a $500 voucher"

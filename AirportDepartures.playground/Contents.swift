3f83741554c6ad3e5005e1854ca1e7b3e9263713
Richard Gibbs <richgibbs@Richs-MacBook.attlocal.net>
2020-05-06 11:53:45 -0700

    completed task a and b

--- AirportDepartures.playground/Contents.swift
+++ AirportDepartures.playground/Contents.swift
@@ -16,9 +16,19 @@ import UIKit
 //: e. Use a `String?` for the Terminal, since it may not be set yet (i.e.: waiting to arrive on time)
 //:
 //: f. Use a class to represent a `DepartureBoard` with a list of departure flights, and the current airport
+enum FLightStatus: String {
+    case enRoute = "En Route"
+    case scheduled = "Scheduled"
+    case delayed = "Delayed"
+    case onTime = "On Time"
+    case cancelled = "Cancelled"
+}
 
-// something
-
+struct Airport {
+    let airportName: String
+    let destination: String
+    let arrival: String
+}
 //: ## 2. Create 3 flights and add them to a departure board
 //: a. For the departure time, use `Date()` for the current time
 //:

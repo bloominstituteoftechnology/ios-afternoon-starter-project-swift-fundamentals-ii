2467714174c699497b8b8c69d1c2b886ee792b67
Richard Gibbs <richgibbs@Richs-MacBook.attlocal.net>
2020-05-06 15:02:27 -0700

    somehow lost step 3 n 4, fixed that

--- AirportDepartures.playground/Contents.swift
+++ AirportDepartures.playground/Contents.swift
@@ -66,6 +66,33 @@ departureFlights.append(contentsOf: [flight3])
 
 let departingFlights = DepartureBoard(departureFlights: departureFlights)
 
+//: ## 3. Create a free-standing function that can print the flight information from the `DepartureBoard`
+//: a. Use the function signature: `printDepartures(departureBoard:)`
+//:
+//: b. Use a `for in` loop to iterate over each departure
+//:
+//: c. Make your `FlightStatus` enum conform to `String` so you can print the `rawValue` String values from the `enum`. See the [enum documentation](https://docs.swift.org/swift-book/LanguageGuide/Enumerations.html).
+//:
+//: d. Print out the current DepartureBoard you created using the function
+
+
+
+
+//: ## 4. Make a second function to print print an empty string if the `departureTime` is nil
+//: a. Createa new `printDepartures2(departureBoard:)` or modify the previous function
+//:
+//: b. Use optional binding to unwrap any optional values, use string interpolation to turn a non-optional date into a String
+//:
+//: c. Call the new or udpated function. It should not print `Optional(2019-05-30 17:09:20 +0000)` for departureTime or for the Terminal.
+//:
+//: d. Stretch: Format the time string so it displays only the time using a [`DateFormatter`](https://developer.apple.com/documentation/foundation/dateformatter) look at the `dateStyle` (none), `timeStyle` (short) and the `string(from:)` method
+//:
+//: e. Your output should look like:
+//:
+//:     Destination: Los Angeles Airline: Delta Air Lines Flight: KL 6966 Departure Time:  Terminal: 4 Status: Canceled
+//:     Destination: Rochester Airline: Jet Blue Airways Flight: B6 586 Departure Time: 1:26 PM Terminal:  Status: Scheduled
+//:     Destination: Boston Airline: KLM Flight: KL 6966 Departure Time: 1:26 PM Terminal: 4 Status: Scheduled
+
 
 
 //: ## 5. Add an instance method to your `DepatureBoard` class (above) that can send an alert message to all passengers about their upcoming flight. Loop through the flights and use a `switch` on the flight status variable.

fe679ffd12cc8f9e65a0a8906b74a565fd16c005
Richard Gibbs <richgibbs@Richs-MacBook.attlocal.net>
2020-05-05 22:53:00 -0700

    testing my capabilities

--- AirportDepartures.playground/Contents.swift
+++ AirportDepartures.playground/Contents.swift
@@ -17,7 +17,7 @@ import UIKit
 //:
 //: f. Use a class to represent a `DepartureBoard` with a list of departure flights, and the current airport
 
-
+// something
 
 //: ## 2. Create 3 flights and add them to a departure board
 //: a. For the departure time, use `Date()` for the current time
